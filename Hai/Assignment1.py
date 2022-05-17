aca0a35d250569422ff26bff0c03198c3164fdaf
hh1207 <96813406+hh1207@users.noreply.github.com>
2022-05-17 16:41:02 -0400

    Solving Keyword/Identifier Problem

---
+++ Hai/Assignment1.py
@@ -0,0 +1,38 @@
+#This will check if the user input is an identifier or not. The while loop will continue
+#until the user  provides a valid input. 
+
+# import keyword
+
+# check = False
+# while(check==False):
+
+#     userInput1 = input("Check if this is a keyword: ")
+#     var = userInput1
+
+#     if(keyword.iskeyword(var)):
+#      print("This is a Python Keyword:")
+#      check = True
+#     else:
+#      print("This is not a keyword try again")
+ 
+#====================================
+
+
+#This will check if the user input is an identifier or not. The while loop will continue
+#until the user  provides a valid input. 
+
+# check = False
+# while(check==False):
+
+#     userInput1 = input("Check if this is a Identifier: ")
+#     var = userInput1
+
+#     if(userInput1.isidentifier()):
+#      print("This is an Identifier:")
+#      check = True
+#     else:
+#      print("This is not an identifier try again")
+ 
+
+
+
